c238ecc4dd4a32cd30fb36e07f415c6590b99378
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-09-08 22:49:49 +0300

    testing

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,2 +1,4 @@
-from rest_framework import APiTestCase
+from rest_framework import APiTestCase,status,response.data
+
 "self", "class", "APITestCase"
+"from rest_framework import status", "response.data"
