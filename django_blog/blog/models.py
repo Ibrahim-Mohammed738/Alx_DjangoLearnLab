6b18d0cca90512a2ab26d1b16448b08a424f6fd7
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-09-12 18:38:23 +0300

    Initial Setup-Blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
-class User(AbstractUser):
-    email = models.EmailField(max_length=150, blank=True, null=True)
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
