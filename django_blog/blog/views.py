365558153eda1f8df09736820409f099ba6cdaac
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-09-15 16:17:43 +0300

    Blog Post Management

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, render
