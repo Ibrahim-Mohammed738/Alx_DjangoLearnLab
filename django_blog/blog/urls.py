5be77a238dc4099623c5c360d56b91edd9f713eb
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-09-16 00:19:56 +0300

    Comment Functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,12 +29,12 @@ urlpatterns = [
     path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path(
-        "posts/<int:post_id>/comments/new/",
+        "post/<int:post_id>/comments/new/",
         CommentCreateView.as_view(),
         name="comment-create",
     ),
-    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-update"),
     path(
-        "comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"
+        "comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"
     ),
 ]
