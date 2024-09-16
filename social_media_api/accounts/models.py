8de87785cd65d1c3833dc0acf990c0759dec7ed2
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-09-16 21:53:19 +0300

    User Authentication

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,12 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=255,null=True,blank=True)
-    profile_picture = models.ImageField(blank=True,null=True)
-    followers = models.ManyToManyField('self',symmetrical=False,blank=True,related_name='following')
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(blank=True, null=True)
+    followers = models.ManyToManyField(
+        "self", symmetrical=False, blank=True, related_name="following"
+    )
 
     def __str__(self):
         return self.username
-    
-
-    
